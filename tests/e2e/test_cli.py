45cdef4aa5ef2deeb23e470c0050106176e2f579
watarukura <what.r.j150@gmail.com>
2020-07-03 22:31:58 +0900

    add e2e test

--- tests/e2e/test_cli.py
+++ tests/e2e/test_cli.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from click.testing import CliRunner
 
 from src.main import cli
@@ -7,7 +8,9 @@ runner = CliRunner()
 e2e_test_dir = "tests/e2e/xlsx/"
 
 
-def test_cli_csv(tmpdir):
+def test_cli(tmpdir):
+    tmp_dir_name = tmpdir.dirname
+
     runner.invoke(
         cli,
         args=[
@@ -16,15 +19,40 @@ def test_cli_csv(tmpdir):
             1,
             "a1",
             f"{e2e_test_dir}input.csv",
-            tmpdir.join("output.xlsx"),
+            f"{tmp_dir_name}/output_from_csv.xlsx",
             "--delimiter",
             ",",
         ],
     )
-    assert tmpdir.join("/output.xlsx").isfile
 
+    df_expected = pd.read_excel(f"{e2e_test_dir}output.xlsx", index_col=0)
+    df_from_csv = pd.read_excel(
+        f"{tmp_dir_name}/output_from_csv.xlsx", index_col=0
+    )
 
-def test_cli_ssv(tmpdir):
+    assert df_expected.equals(df_from_csv)
+
+    runner.invoke(
+        cli,
+        args=[
+            f"{e2e_test_dir}output.xlsx",
+            "-T",
+            1,
+            "f6",
+            f"{e2e_test_dir}input.ssv",
+            f"{tmp_dir_name}/output_from_ssv.xlsx",
+            "--delimiter",
+            " ",
+        ],
+    )
+
+    df_expected2 = pd.read_excel(f"{e2e_test_dir}output2.xlsx", index_col=0)
+    df_from_ssv = pd.read_excel(
+        f"{tmp_dir_name}/output_from_ssv.xlsx", index_col=0
+    )
+
+    assert df_expected2.equals(df_from_ssv)
+
     runner.invoke(
         cli,
         args=[
@@ -32,10 +60,19 @@ def test_cli_ssv(tmpdir):
             "-T",
             1,
             "a1",
-            f"{e2e_test_dir}input.ssv",
-            tmpdir.join("output.xlsx"),
+            f"{e2e_test_dir}input.tsv",
+            "-T",
+            1,
+            "f6",
+            f"{e2e_test_dir}input.tsv",
+            f"{tmp_dir_name}/output_from_tsv.xlsx",
             "--delimiter",
-            " ",
+            "\t",
         ],
     )
-    assert tmpdir.join("/output.xlsx").isfile
+
+    df_from_tsv = pd.read_excel(
+        f"{tmp_dir_name}/output_from_tsv.xlsx", index_col=0
+    )
+
+    assert df_expected2.equals(df_from_tsv)
