8b206f9aa7bdd69228e7d766e41a1d9583df98db
watarukura <what.r.j150@gmail.com>
2020-07-05 04:46:08 +0900

    仮実装

--- src/main.py
+++ src/main.py
@@ -3,14 +3,19 @@ from typing import Any, List
 
 import click
 
-from src.excel import write_xlsx
+from src.excel import read_xlsx, write_xlsx
 
 
 def main() -> None:
     sys.exit(cli())
 
 
-@click.command()
+@click.group(help="openpyxl wrappert cli")
+def cli() -> None:
+    pass
+
+
+@cli.command()
 @click.argument("template_xlsx", required=True, type=click.Path(exists=True))
 @click.option(
     "--sheet_xy_csv",
@@ -23,13 +28,30 @@ def main() -> None:
 )
 @click.argument("output_xlsx", required=True, type=click.Path(writable=True))
 @click.option("--delimiter", default=",", type=str, help="csv delimiter")
-def cli(
+def write(
     template_xlsx: str,
     sheet_xy_csv: List[Any],
     output_xlsx: str,
     delimiter: str,
 ) -> None:
     write_xlsx(template_xlsx, sheet_xy_csv, output_xlsx, delimiter)
+
+
+@cli.command()
+@click.option(
+    "--input_xlsx", "-I", required=True, type=click.Path(readable=True)
+)
+@click.option("--sheet_no", default=1, type=int, help="sheet no")
+@click.option("--delimiter", default=",", type=str, help="csv delimiter")
+def read(input_xlsx: str, sheet_no: int, delimiter=str) -> None:
+    values = read_xlsx(input_xlsx, sheet_no)
+
+    for row in values:
+        print(*row, sep=delimiter)
+
+
+cli.add_command(write)
+cli.add_command(read)
 
 
 if __name__ == "__main__":
